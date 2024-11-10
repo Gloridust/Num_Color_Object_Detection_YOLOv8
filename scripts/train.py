41d9304d757814bff3dcb48df82891794408f726
Gloridust <Gloridust@gmail.com>
2024-11-11 01:07:15 +0800

    update

--- scripts/train.py
+++ scripts/train.py
@@ -1,12 +1,16 @@
 from ultralytics import YOLO
+import os
 
 def train_model():
+    # 设置工作目录为项目根目录
+    os.chdir('/home/gloridust/Documents/Github/Num_Color_Object_Detection_YOLOv8')
+
     # 加载YOLOv8的nano模型
     model = YOLO('yolov8n.yaml')
 
     # 开始训练
     model.train(
-        data='data.yaml',  # 数据集配置文件
+        data='/home/gloridust/Documents/Github/Num_Color_Object_Detection_YOLOv8/data.yaml',  # 使用绝对路径
         epochs=50,
         imgsz=640,
         batch=16,
